e86f8dbdca6113cdf30fb8bce7241d3521c20867
Matt White <matt@ldcvia.com>
2015-06-04 14:11:07 +0100

    Add link for individual entries

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ var rest = require('restler');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("Getting home page...");
-  // direct way
-
   rest.get('https://eu.ldcvia.com/1.0/collections/blog/entry', {headers: {'apikey': 'ffcb216445defbf0450dc1719a5a5117'}})
     .on('timeout', function(ms){
       console.log('did not return within '+ms+' ms');
@@ -14,9 +11,16 @@ router.get('/', function(req, res, next) {
     .on('complete',function(data,response){
       res.render('index', { title: 'Blog', entries: data });
     });
+});
 
-
-
+router.get('/:id', function(req, res, next){
+  rest.get('https://eu.ldcvia.com/1.0/document/blog/entry/' + req.params.id, {headers: {'apikey': 'ffcb216445defbf0450dc1719a5a5117'}})
+    .on('timeout', function(ms){
+      console.log('did not return within '+ms+' ms');
+    })
+    .on('complete',function(data,response){
+      res.render('entry', { title: data.title, entry: data });
+    });
 });
 
 module.exports = router;
