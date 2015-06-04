7bacddeef81c1efa936f89d391e0328e34326690
Matt White <matt@ldcvia.com>
2015-06-04 13:58:48 +0100

    Add moment support for dates

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
