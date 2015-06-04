17e8808b97bee2c0f9b93bed5f54523e9b2c0a11
Matt White <matt@ldcvia.com>
2015-06-04 15:45:41 +0100

    Move config out to file

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  hostname: "https://eu.ldcvia.com/1.0",
+  publicapikey: 'ffcb216445defbf0450dc1719a5a5117'
+};
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var rest = require('restler');
+var app = express();
+var config = require('../config');
+var count = 5;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  rest.get('https://eu.ldcvia.com/1.0/collections/blog/entry', {headers: {'apikey': 'ffcb216445defbf0450dc1719a5a5117'}})
-    .on('timeout', function(ms){
-      console.log('did not return within '+ms+' ms');
-    })
-    .on('complete',function(data,response){
-      res.render('index', { title: 'Blog', entries: data });
-    });
+  console.log(config.hostname + '/collections/blog/entry?count=' + count);
+  rest.get(config.hostname + '/collections/blog/entry?count=' + count, {headers: {'apikey': config.publicapikey}})
+  .on('complete',function(data,response){
+    console.log(data);
+    res.render('index', { title: 'Blog', entries: data, count: count, next: 1, previous: 0 });
+  });
 });
 
-router.get('/:id', function(req, res, next){
-  rest.get('https://eu.ldcvia.com/1.0/document/blog/entry/' + req.params.id, {headers: {'apikey': 'ffcb216445defbf0450dc1719a5a5117'}})
-    .on('timeout', function(ms){
-      console.log('did not return within '+ms+' ms');
-    })
-    .on('complete',function(data,response){
-      res.render('entry', { title: data.title, entry: data });
-    });
+router.get('/page/:pageno', function(req, res, next) {
+  var start = req.params.pageno * count;
+  rest.get(config.hostname + '/collections/blog/entry?count=' + count + '&start=' + start, {headers: {'apikey': config.publicapikey}})
+  .on('complete',function(data,response){
+    res.render('index', { title: 'Blog', entries: data, count: count, next: parseInt(req.params.pageno, 10) + 1, previous: parseInt(req.params.pageno, 10) - 1 });
+  });
+});
+
+router.get('/blog/:id', function(req, res, next){
+  rest.get(config.hostname + '/document/blog/entry/' + req.params.id, {headers: {'apikey': config.publicapikey}})
+  .on('complete',function(data,response){
+    res.render('entry', { title: data.title, entry: data });
+  });
 });
 
 module.exports = router;
